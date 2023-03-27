fc0d6275a1fd061044fa6c6da090df8429e42bbc
car <108854756+yahlol@users.noreply.github.com>
2023-03-27 18:25:19 -0400

    Update table.lua

--- table.lua
+++ table.lua
@@ -1,3 +1,38 @@
+--[[
+    
+     _    _                 _     _____ _             ____            _       _   
+    / \  | |__   ___  _   _| |_  |_   _| |__   ___   / ___|  ___ _ __(_)_ __ | |_ 
+   / _ \ | '_ \ / _ \| | | | __|   | | | '_ \ / _ \  \___ \ / __| '__| | '_ \| __|
+  / ___ \| |_) | (_) | |_| | |_    | | | | | |  __/   ___) | (__| |  | | |_) | |_ 
+ /_/   \_\_.__/ \___/ \__,_|\__|   |_| |_| |_|\___|  |____/ \___|_|  |_| .__/ \__|
+                                                                       |_|        
+           - We offer full customzablity to all users who use the script
+           - We Also give options like aim viewer and two types of anti aim
+           - Constant Updates (table will update at some points)
+           - For Updates Please Check: https://github.com/yahlol/OP.WIN/tree/main
+           ----------------------------------------------------------------
+
+     _          _   _      _    _              ___        _   _                 
+    / \   _ __ | |_(_)    / \  (_)_ __ ___    / _ \ _ __ | |_(_) ___  _ __  ___ 
+   / _ \ | '_ \| __| |   / _ \ | | '_ ` _ \  | | | | '_ \| __| |/ _ \| '_ \/ __|
+  / ___ \| | | | |_| |  / ___ \| | | | | | | | |_| | |_) | |_| | (_) | | | \__ \
+ /_/   \_\_| |_|\__|_| /_/   \_\_|_| |_| |_|  \___/| .__/ \__|_|\___/|_| |_|___/
+                                                   |_|                          
+                        
+
+          [+] Behind
+          [+] Down -- can easily be resolved
+          [+] Forward
+          [+] Left
+          [+] One
+          [+] Right
+          [+] Up -- can easily be resolved
+          [+] Zero
+
+          NOTE: Each anti aim method is customizable to your liking except 'Zero'
+          ----------------------------------------------------------------
+--]]
+
 getgenv().OP_WIN = {
     AntiAim = {
         ["Enable"] = true,
