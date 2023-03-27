10f77158bc74c4209a0467ae1ebbe44b97ff08e4
car <108854756+yahlol@users.noreply.github.com>
2023-03-27 03:24:34 -0400

    Update table.lua

--- table.lua
+++ table.lua
@@ -3,18 +3,5 @@ getgenv().OP_WIN = {
         ["Enable"] = true,
         ["Keybind"] = Enum.KeyCode.X,
         ["AntiMethod"] = "Behind"
-    },
-    AimViewer = {
-        ["Enable"] = true,
-        ["Toggle"] = "p",
-        ["ViewPlayer"] = "b",
-        ["Color"] = Color3.fromRGB(255,0,0)
-    },
-    CustomVelocity = {
-        ["Enable"] = false,
-        ["Keybind"] = Enum.KeyCode.V,
-        ["X"] = 9304,
-        ["Y"] = -3929,
-        ["Z"] = 5940
     }
 }
