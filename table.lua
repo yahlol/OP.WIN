253d1f7c0cebf203d480644887174750e8a6ccbb
car <108854756+yahlol@users.noreply.github.com>
2023-03-27 03:05:36 -0400

    Create table.lua

---
+++ table.lua
@@ -0,0 +1,20 @@
+getgenv().OP_WIN = {
+    AntiAim = {
+        ["Enable"] = true,
+        ["Keybind"] = Enum.KeyCode.X,
+        ["AntiMethod"] = "Behind"
+    },
+    AimViewer = {
+        ["Enable"] = true,
+        ["Toggle"] = "p",
+        ["ViewPlayer"] = "b",
+        ["Color"] = Color3.fromRGB(255,0,0)
+    },
+    CustomVelocity = {
+        ["Enable"] = false,
+        ["Keybind"] = Enum.KeyCode.V,
+        ["X"] = 9304,
+        ["Y"] = -3929,
+        ["Z"] = 5940
+    }
+}
