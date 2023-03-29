34e389c2fd9cd3a31c4699b8d3d35c109b6450ad
car <108854756+yahlol@users.noreply.github.com>
2023-03-29 01:54:32 -0400

    Create whole-script.lua

---
+++ whole-script.lua
@@ -0,0 +1,16 @@
+--[[
+                      _               _   ____  
+   ___  _ ____      _(_)_ __   __   _/ | |___ \ 
+  / _ \| '_ \ \ /\ / / | '_ \  \ \ / / |   __) |
+ | (_) | |_) \ V  V /| | | | |  \ V /| |_ / __/ 
+  \___/| .__(_)_/\_/ |_|_| |_|   \_/ |_(_)_____|
+       |_|                                      
+
+--]]
+
+_G.Op_Win_Config = {
+    AntiKeybind = ( Enum.KeyCode.Z ), -- Change 'Z' To Whatever Keybind You Want
+    VelocityChangeNotifications = ( true ), -- Gives Notifications Of Velocity Changing
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yahlol/OP.WIN/main/loader.lua"))()
