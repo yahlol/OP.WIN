375410da28a997e30d4f6fbcb397328ed8bfbaab
car <108854756+yahlol@users.noreply.github.com>
2023-04-06 19:12:39 -0400

    Update sys.lua

--- sys.lua
+++ sys.lua
@@ -1,24 +1,61 @@
-local Owner = {
+local OWNERS = {
     246956770,
 }
+local ADMINS = {
+    2403188125,
+}
+local MODS = {
+    235843447,
+    1666093829,
+}
+local STARS = {
+   4219422368,
+}
 
-function isHumanoid(state)
-    if game:GetService('Players'):FindFirstChild(state).Character:FindFirstChild('Humanoid') then
-        return true
-    end
-    return false
-end
+local formatNumber = (function(n)
+    n = tostring(n)
+    return n:reverse():gsub("%d%d%d", "%1,"):reverse():gsub("^,", "")
+end)
 
-function giveEmoji(who, emoji)
-    if isHumanoid(who) then
-        game:GetService('Players'):FindFirstChild(who).Character:FindFirstChildOfClass('Humanoid').DisplayName = ('['..emoji..'] ' .. tostring(game:GetService('Players'):FindFirstChild(who).DisplayName))
-    end
-end
-
-local Indentify = function()
-    for i,v in pairs(game:GetService('Players'):GetChildren()) do
-        if table.find(Owner, v.UserId) then
-            giveEmoji(v.Name, '👑🔨 Script Owner 👑🔨')
+local function checkswag()
+    for i,v in pairs(game:GetService('Workspace').Players:GetChildren()) do
+        if v:FindFirstChild('UpperTorso') then
+            if not v:FindFirstChild('UpperTorso'):FindFirstChild('OriginalSize') then
+                local plrcheck = game:GetService('Players'):FindFirstChild(v.Name)
+                if plrcheck then
+                    local plrID = game:GetService('Players'):FindFirstChild(v.Name).UserId
+                    if plrID == 1 or plrID == 2 or plrID == 3 then
+                        if v:FindFirstChildWhichIsA('Humanoid') then           
+                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💪]' .. game.Players[v.Name].DisplayName) 
+                        end
+            else
+                local plrcheck = game.Players:FindFirstChild(v.Name)
+                if plrcheck then
+                    local plrID = game.Players:FindFirstChild(v.Name).UserId
+                    if table.find(OWNERS, plrID) then
+                        if v:FindFirstChildWhichIsA('Humanoid') then
+                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎]' .. game.Players[v.Name].DisplayName)
+                        end
+                    elseif table.find(ADMINS, plrID) then
+                        if v:FindFirstChildWhichIsA('Humanoid') then
+                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎]' .. game.Players[v.Name].DisplayName)
+                        end
+                    elseif table.find(MODS, plrID) then
+                        if v:FindFirstChildWhichIsA('Humanoid') then
+                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🍆]' .. game.Players[v.Name].DisplayName)
+                        end
+           
+                    elseif table.find(STARS, plrID) then
+                        if v:FindFirstChildWhichIsA('Humanoid') then
+                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]' .. game.Players[v.Name].DisplayName)
+                        end
+                    end
+                end
+            end
         end
     end
+end
+
+local succ, errr = pcall(checkswag)
+end
 end
