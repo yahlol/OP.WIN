6028e74714f0739a80a7001b611a8a2eff2ae793
car <108854756+yahlol@users.noreply.github.com>
2023-04-06 18:36:47 -0400

    Create sys.lua

---
+++ sys.lua
@@ -0,0 +1,24 @@
+local Owner = {
+    246956770,
+}
+
+function isHumanoid(state)
+    if game:GetService('Players'):FindFirstChild(state).Character:FindFirstChild('Humanoid') then
+        return true
+    end
+    return false
+end
+
+function giveEmoji(who, emoji)
+    if isHumanoid(who) then
+        game:GetService('Players'):FindFirstChild(who).Character:FindFirstChildOfClass('Humanoid').DisplayName = ('['..emoji..'] ' .. tostring(game:GetService('Players'):FindFirstChild(who).DisplayName))
+    end
+end
+
+local Indentify = function()
+    for i,v in pairs(game:GetService('Players'):GetChildren()) do
+        if table.find(Owner, v.UserId) then
+            giveEmoji(v.Name, '👑🔨 Script Owner 👑🔨')
+        end
+    end
+end
